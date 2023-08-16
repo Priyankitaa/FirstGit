fede90be6eb5f521da5564787656c5ac641519d6
Priyankitaa <riyasaxena1998@gmail.com>
2023-08-16 17:00:22 +0530

    dom task3 added

---
+++ DOM/task4.js
@@ -0,0 +1,8 @@
+var listItems = document.getElementsByClassName('list-group-item');
+
+listItems[2].style.backgroundColor = 'green';
+
+for(var i = 0; i < listItems.length; i++)
+{
+    listItems[i].style.fontWeight = 'bold';
+}
